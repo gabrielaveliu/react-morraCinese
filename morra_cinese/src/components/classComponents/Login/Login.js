d665428147e74aa44c617fe01a5511c125e090ef
Alessandro Veneziano <aveneziano536@gmail.com>
2022-01-14 00:43:12 +0100

    login its finish

--- morra_cinese/src/components/classComponents/Login/Login.js
+++ morra_cinese/src/components/classComponents/Login/Login.js
@@ -1,37 +1,73 @@
+import "./Login.css"
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
-
+/* componets func */
+import Button from "../../funcComponent/Button/Button";
 
 class Login extends Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+            animationClassLogin: ""
+        }
         this.valueInput = ""
+    }
+    setAnimation = () => {
+        this.setState({
+            animationLogin: "hiding_tab_login"
+        })
     }
 
     handlerChangeInput = (e) => {
         this.valueInput = e.target.value;
     }
     handlerClicklogin = () => {
-        if (this.props.callback !== undefined && this.valueInput.length > 0) {
-            this.props.callback(false, this.valueInput)
-        }
+        this.setAnimation()
+
+        setInterval(() => {
+            if (this.props.callback !== undefined && this.valueInput.length > 0) {
+                this.props.callback(false, this.valueInput)
+            }
+        }, 500);
     }
     handlerClickAnonimus = () => {
-        if (this.props.callback !== undefined) {
-            this.props.callback(false)
+        this.setAnimation()
 
-        }
+        setInterval(() => {
+            if (this.props.callback !== undefined) {
+                this.props.callback(false)
+
+            }
+        }, 500);
     }
-
 
     render() {
         return (
             <div className='container_login'>
-                <input onChange={this.handlerChangeInput} type={"text"}></input>
-                <button onClick={this.handlerClicklogin}> fai login</button>
-                <button onClick={this.handlerClickAnonimus}>accedi in anonimo</button>
+                <div className={`box_login  startBoxLog ${this.state.animationClassLogin}`}>
+                    <div className='box_inputs'>
+                        <div className="login_input_tag">
+                            <input placeholder="NickName" onChange={this.handlerChangeInput} type={"text"}></input>
+                        </div>
+                        <div className={"login_loginbutton_tag"}>
+                            <Button
+                                onClick={this.handlerClicklogin}
+                                label="fai login"
+                                specialClass="button_log"
+                            />
+                        </div>
+                        <div className={"login_anonimusbutton_tag"}>
+                            <Button
+                                onClick={this.handlerClickAnonimus}
+                                label="accedi in anonimo"
+                                specialClass="button_anonimus"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
