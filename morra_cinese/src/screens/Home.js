de99c02a6c90ac363b8a0ea8f029080e47a809df
Alessandro Veneziano <aveneziano536@gmail.com>
2022-01-13 11:38:53 +0100

    first_step

---
+++ morra_cinese/src/screens/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+
+    /* da qui ci collegheremo a LOgin e CoreGame */
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
