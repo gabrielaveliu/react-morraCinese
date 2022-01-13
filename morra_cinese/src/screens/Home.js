3fb2378f9d6b66c9fe3e92e5c00609878ab7b59e
Gabriela Veliu <gabriela.veliu2@gmail.com>
2022-01-13 11:41:44 +0100

    Constructor

--- morra_cinese/src/screens/Home.js
+++ morra_cinese/src/screens/Home.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 
 class Home extends Component {
+    constructor(props){
+        super(props);
+        
+    }
 
     /* da qui ci collegheremo a LOgin e CoreGame */
     render() {
